cffe9cecd13f0108ef5cdda84db892968dc10642
STENUNGSUND\frida.eriksson <frida_eriksson@live.se>
2017-09-28 16:20:56 +0200

    Pretty done

---
+++ Motivation/Static/Js/cookieresponse.js
@@ -0,0 +1,49 @@
+﻿
+$(".cookie").click(function () {
+    $(this).addClass("hidden");
+    $(".cookieResponse").removeClass("hidden");
+})
+
+//ALL OTHER FUNCTIONALITY MUST BE LOADED BEFORE THE COUNTER
+//ONLY FUNCTIONS THAT ARE CALLED FROM WITHIN THE COUNTER CAN BE BELOW
+$(function () {
+    hideAll();
+    var counter = 0;
+    var myInterval = setInterval(function () {
+        
+        if (counter === 20) {
+            var h1 = "20 minutes reward";
+            var p = "Your blood pressure, pulse rate and the temperature of your hands and feet have returned to normal.";
+            cookieReward(h1, p);
+        }
+
+        if (counter === 60) {
+            hideAll();
+            var h1 = "1 hour reward";
+            giftReward(h1, p);
+        }
+
+
+        ++counter;
+    }, 1000);
+})
+
+// to stop the counter
+clearInterval(myInterval);
+
+function hideAll() {
+    $(".cookieResponse").addClass("hidden");
+    $(".cookie").addClass("hidden");
+    $("#present").addClass("hidden");
+}
+
+function cookieReward(h1, p) {
+    $("h1").html(h1);
+    $(".cookieResponse").html(p);
+    $(".cookie").removeClass("hidden");
+}
+
+function giftReward(h1) {
+    $("h1").html(h1);
+    $("#present").removeClass("hidden");
+}
--- Motivation/Static/Js/main.js
+++ Motivation/Static/Js/main.js
@@ -1,11 +1,6 @@
-﻿$("#closedPresent").click(function () {
-    $("#closedPresent").addClass("hidden")
-    $("#bottomPresent").removeClass("hidden")
-    $("#topPresent").removeClass("hidden")
+﻿$("#present").click(function () {
     setTimeout(function () {
         ($("#topPresent").addClass("topOff"))
         
     }, 50);
-    ($("#topPresent").addClass("stayUp"))
-    
 })
