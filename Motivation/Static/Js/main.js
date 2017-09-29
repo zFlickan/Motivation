b201fc9e2bcf90bdbc89edd23ab2d35fe68ca181
STENUNGSUND\anders.bjorn <anders.bjorn@outlook.com>
2017-09-29 14:01:23 +0200

    lade till siffror på antal ädelstenar

--- Motivation/Static/Js/main.js
+++ Motivation/Static/Js/main.js
@@ -5,32 +5,76 @@
     getRandomGem()
 })
 
+$("#treasureChest").click(function toggleTreasure() {
+    var element = document.getElementById("treasureDiv");
+    element.classList.toggle("hidden");
+    element.classList.toggle("treasureContent");
+})
+
+var emeraldCount = 0;
+var sapphireCount = 0;
+var rubyCount = 0;
+var diamondCount = 0;
+
 function getRandomGem() {
     var x = Math.floor((Math.random() * 4) + 1);
     var gem;
     switch (x) {
         case 1:
             $("#emerald").removeClass("hidden");
-            gem = "emerald";
+            gem = "emerald"
+            storedGem = "#storedEmerald"
+            emeraldCount++;
+            $("#emeraldCount").html(emeraldCount)
+            $("#emeraldCount").removeClass("hidden")
             break;
         case 2: $("#diamond").removeClass("hidden");
-            gem = "diamond";
+            gem = "diamond"
+            storedGem = "#storedDiamond"
+            diamondCount++;
+            $("#diamondCount").html(diamondCount)
+            $("#diamondCount").removeClass("hidden")
             break;
         case 3:
             $("#ruby").removeClass("hidden");
-            gem = "ruby";
+            gem = "ruby"
+            storedGem = "#storedRuby"
+            rubyCount++;
+            $("#rubyCount").html(rubyCount)
+            $("#rubyCount").removeClass("hidden")
             break;
         case 4: $("#sapphire").removeClass("hidden");
-            gem = "sapphire";
+            gem = "sapphire"
+            storedGem = "#storedSapphire"
+            sapphireCount++;
+            $("#sapphireCount").html(sapphireCount)
+            $("#sapphireCount").removeClass("hidden")
             break;
         default: $("#diamond").removeClass("hidden");
-            gem = "diamond";
+            gem = "diamond"
+            storedGem = "#storedDiamond"
+            diamondCount++;
+            $("#diamondCount").html(diamondCount)
+            $("#diamondCount").removeClass("hidden")
             break;
     }
-    animateGem(gem)
+    //transitGem(gem, storedGem)
+    animateGem(gem, storedGem)
 }
 
-function animateGem(gem) {
+//function transitGem(gem, storedGem) {
+//    var elem = document.getElementById(gem);
+//    var p = $("#treasureChest");
+//    var position = p.offset();
+//    var elem = document.getElementById(gem);
+//    var boxTop = Math.floor($(gem).offset().top);
+//    var boxLeft = Math.floor($(gem).offset().left);
+//    var chestTop = Math.floor(position.top);
+//    var chestLeft = Math.floor(position.left);
+//    $(gem).css("translation", "left 2s", `${boxLeft - chestLeft}` + 'px', "top 2s", `${boxTop - chestTop}` + 'px')
+//}
+
+function animateGem(gem, storedGem) {
     var p = $("#treasureChest");
     var position = p.offset();
     var elem = document.getElementById(gem);
@@ -50,6 +94,10 @@ function animateGem(gem) {
             clearInterval(id);
             if (topFinished && leftFinished) {
                 $(elem).addClass("hidden")
+                elem.style.top = 0 + 'px'
+                elem.style.left = 0 + 'px'
+                $(storedGem).removeClass("hidden")
+                $(storedGem).addClass("block")
             }
         } else {
             toppos++;
@@ -68,6 +116,10 @@ function animateGem(gem) {
             clearInterval(id2);
             if (topFinished && leftFinished) {
                 $(elem).addClass("hidden")
+                elem.style.top = 0 + 'px'
+                elem.style.left = 0 + 'px'
+                $(storedGem).removeClass("hidden")
+                $(storedGem).addClass("block")
             }
         } else {
             leftpos++;
