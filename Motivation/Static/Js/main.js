e8f40a7a37fea23932c7dc2434dc0b656f794645
STENUNGSUND\anders.bjorn <anders.bjorn@outlook.com>
2017-09-29 16:03:07 +0200

    tog bort cssanimation la till klick på kistan

--- Motivation/Static/Js/main.js
+++ Motivation/Static/Js/main.js
@@ -22,8 +22,6 @@ var sapphireCount = 0;
 var rubyCount = 0;
 var diamondCount = 0;
 
-
-
 function getRandomGem() {
     var x = Math.floor((Math.random() * 4) + 1);
     var gem;
@@ -31,14 +29,14 @@ function getRandomGem() {
     switch (x) {
         case 1:
             $("#emerald").removeClass("hidden");
-            gem = "#emerald"
+            gem = "emerald"
             storedGem = "#storedEmerald"
             emeraldCount++;
             count = "#emeraldCount";
             $("#emeraldCount").html(emeraldCount)
             break;
         case 2: $("#diamond").removeClass("hidden");
-            gem = "#diamond"
+            gem = "diamond"
             storedGem = "#storedDiamond"
             diamondCount++;
             count = "#diamondCount";
@@ -46,62 +44,29 @@ function getRandomGem() {
             break;
         case 3:
             $("#ruby").removeClass("hidden");
-            gem = "#ruby"
+            gem = "ruby"
             storedGem = "#storedRuby"
             rubyCount++;
             count = "#rubyCount";
             $("#rubyCount").html(rubyCount)
             break;
         case 4: $("#sapphire").removeClass("hidden");
-            gem = "#sapphire"
+            gem = "sapphire"
             storedGem = "#storedSapphire"
             sapphireCount++;
             count = "#sapphireCount";
             $("#sapphireCount").html(sapphireCount)
             break;
         default: $("#diamond").removeClass("hidden");
-            gem = "#diamond"
+            gem = "diamond"
             storedGem = "#storedDiamond"
             diamondCount++;
             count = "#diamondCount";
             $("#diamondCount").html(diamondCount)
             break;
     }
-    //animateGem(gem, storedGem, count)
-    cssAnimation(gem, storedGem, count)
+    animateGem(gem, storedGem, count)
 }
-function cssAnimation(gem, storedGem, count) {
-    var p = $("#treasureChest");
-    var position = p.offset();
-    var boxTop = Math.floor($(gem).offset().top);
-    var boxLeft = Math.floor($(gem).offset().left);
-    var chestTop = Math.floor(position.top);
-    var chestLeft = Math.floor(position.left);
-    var css = `
-@keyframes animateGem {
-    0 % {
-        top: ${boxTop}px;
-        left: ${boxLeft}px;
-    }
-    100 % {
-        top: ${chestTop}px;
-        left: ${chestLeft}px;
-    }
-}
-`;
-    var cssClass = `.animationGem {
-    animation-name: animateGem;
-    animation-iteration-count: 1;
-    animation-duration: 3s;
-    animation-fill-mode: forwards;
-}`;
-    $('<style>' + css + cssClass + '</style>').appendTo('head');
-    $(gem).addClass("animationGem")
-    //$(storedGem).removeClass("hidden")
-    //$(storedGem).addClass("block")
-    $(count).removeClass("hidden")
-}
-
 
 function animateGem(gem, storedGem, count) {
     var p = $("#treasureChest");
@@ -118,7 +83,7 @@ function animateGem(gem, storedGem, count) {
 
     var id = setInterval(frame, 5);
     function frame() {
-        if (toppos > (chestTop - boxTop) || (boxTop - chestTop) > toppos) {
+        if (toppos == (chestTop - boxTop) || (boxTop - chestTop) == toppos) {
             topFinished = true;
             clearInterval(id);
             if (topFinished && leftFinished) {
@@ -130,7 +95,7 @@ function animateGem(gem, storedGem, count) {
                 $(count).removeClass("hidden")
             }
         } else {
-            toppos = toppos + (chestTop / boxTop);
+            toppos++;
             if (chestTop < boxTop) {
                 elem.style.top = -toppos + 'px';
             }
@@ -141,7 +106,7 @@ function animateGem(gem, storedGem, count) {
     }
     var id2 = setInterval(frame2, 5);
     function frame2() {
-        if (leftpos < (chestLeft - boxLeft) || (boxLeft - chestLeft) < leftpos) {
+        if (leftpos == (chestLeft - boxLeft) || (boxLeft - chestLeft) == leftpos) {
             leftFinished = true;
             clearInterval(id2);
             if (topFinished && leftFinished) {
@@ -153,7 +118,7 @@ function animateGem(gem, storedGem, count) {
                 $(count).removeClass("hidden")
             }
         } else {
-            leftpos = leftpos + (boxLeft / chestLeft);
+            leftpos++;
             if (chestLeft < boxLeft) {
                 elem.style.left = -leftpos + 'px';
             }
